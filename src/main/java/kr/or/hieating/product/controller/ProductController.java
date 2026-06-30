a6acf701b0eaf193d483e71a2a584db60ed06446
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-06-30 16:50:11 +0900

    fix(#31): 테스트 추가

--- src/main/java/kr/or/hieating/product/controller/ProductController.java
+++ src/main/java/kr/or/hieating/product/controller/ProductController.java
@@ -9,11 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class ProductController {
-
   private final ProductService productService;
-
   @GetMapping("/product")
-  public String mostPurchasedProducts(Model model){
+  public String mostPurchasedProducts(Model model) {
 
     model.addAttribute("products", productService.findMostPurchasedProducts());
     return "product/products";
