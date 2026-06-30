ccde0385167def6095112762854d8dc51410ff81
jhj2075 <116949689+jhj2075@users.noreply.github.com>
2026-06-30 16:32:30 +0900

    feat(#24): 메인 페이지 top4 상품 조회 기능 구현 (#28)
    * feat: 메인 페이지 top4 상품 조회 기능 구현
    
    * feat: top4 상품 조회 테스트 화면 구현
    
    * fix: spotless 설정 변경
    
    * fix: coderabbit 리뷰 적용

---
+++ src/main/java/kr/or/hieating/product/controller/ProductController.java
@@ -0,0 +1,21 @@
+package kr.or.hieating.product.controller;
+
+import kr.or.hieating.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+
+  private final ProductService productService;
+
+  @GetMapping("/product")
+  public String mostPurchasedProducts(Model model){
+
+    model.addAttribute("products", productService.findMostPurchasedProducts());
+    return "product/products";
+  }
+}
