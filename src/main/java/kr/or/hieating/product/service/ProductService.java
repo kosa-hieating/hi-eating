5d5c3c45573a3893ceee9ad5eec5651a018fb75d
jhj2075 <flg989@naver.com>
2026-06-30 16:09:49 +0900

    feat: 메인 페이지 top4 상품 조회 기능 구현

---
+++ src/main/java/kr/or/hieating/product/service/ProductService.java
@@ -0,0 +1,18 @@
+package kr.or.hieating.product.service;
+
+import java.util.List;
+import kr.or.hieating.product.dto.MostPurchasedProductResponseDto;
+import kr.or.hieating.product.mapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+  private final ProductMapper productMapper;
+
+  public List<MostPurchasedProductResponseDto> findMostPurchasedProducts(){
+    return productMapper.findMostPurchasedProducts();
+  }
+}
