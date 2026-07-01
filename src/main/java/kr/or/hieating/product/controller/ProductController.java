6f250fca819b3bff7bfa9effbc462d92886d18ef
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-07-01 09:59:37 +0900

    fix(#34): 리뷰 작성 화면 구현

--- src/main/java/kr/or/hieating/product/controller/ProductController.java
+++ src/main/java/kr/or/hieating/product/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class ProductController {
+
   private final ProductService productService;
 
   @GetMapping("/product")
