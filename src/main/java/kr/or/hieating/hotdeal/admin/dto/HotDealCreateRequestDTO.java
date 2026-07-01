dd2818a55ed4cb3f4ff8ac9be900d847a7193a6d
heejinKong <gmlwls3379@naver.com>
2026-07-01 21:05:41 +0900

    feat(#40): HotDealCreateRequestDTO 입력 검증 추가

--- src/main/java/kr/or/hieating/hotdeal/admin/dto/HotDealCreateRequestDTO.java
+++ src/main/java/kr/or/hieating/hotdeal/admin/dto/HotDealCreateRequestDTO.java
@@ -1,6 +1,7 @@
 package kr.or.hieating.hotdeal.admin.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
@@ -15,18 +16,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class HotDealCreateRequestDTO {
-  @Size(max = 10, message = "핫딜 제목은 공백 포함 10글자 이하로 입력해주세요.") private String title;
+  @NotNull(message = "핫딜 제목은 필수 입력값입니다.")
+  @Size(max = 10, message = "핫딜 제목은 공백 포함 10글자 이하로 입력해주세요.")
+  private String title;
 
-  @Size(max = 200, message = "핫딜 설명은 공백 포함 200글자 이하로 입력해주세요.") private String description;
+  @Size(max = 200, message = "핫딜 설명은 공백 포함 200글자 이하로 입력해주세요.")
+  private String description;
 
+  @NotNull(message = "핫딜 시작일은 필수 입력값입니다.")
   @JsonFormat(pattern = "yyyy.MM.dd")
   private LocalDate startsAt;
 
+  @NotNull(message = "핫딜 종료일은 필수 입력값입니다.")
   @JsonFormat(pattern = "yyyy.MM.dd")
   private LocalDate endsAt;
 
+  @NotNull(message = "핫딜 할인율은 필수 입력값입니다.")
   private Integer discountRate;
+
+  @NotNull(message = "핫딜 상품 선택은 필수입니다.")
   private List<ProductItemDTO> products;
+
 
   @Getter
   @Builder
