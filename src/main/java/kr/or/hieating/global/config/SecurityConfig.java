41c9d33d2cd6596e9184110e7ba93a7e8c29bdbc
heejin Kong <113762366+heejinkong@users.noreply.github.com>
2026-07-07 00:40:50 +0900

    Update SecurityConfig.java

--- src/main/java/kr/or/hieating/global/config/SecurityConfig.java
+++ src/main/java/kr/or/hieating/global/config/SecurityConfig.java
@@ -43,7 +43,6 @@ public class SecurityConfig {
                         "/css/**",
                         "/js/**",
                         "/images/**",
-                        "admin/**",
                         "/fonts/**") // 추후 코드 병합시 핫딜과 같은 페이지 허용 필요
                     .permitAll()
                     .requestMatchers("/admin/**")
